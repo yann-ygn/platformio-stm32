9c94b4ee337af33901d6096f72f6b2bd1b709f6e
effectspcbs <yann.gaudineau@gmail.com>
2020-11-22 21:52:52 +0100

    20201122/09:52

--- test/src/utils.h
+++ test/src/utils.h
@@ -20,6 +20,18 @@ namespace Utils
     uint32_t getClockFrequency();
 }
 
+class CircularBuffer
+{
+    private:
+        size_t m_size = 0;
+        size_t m_capacity = 0;
+        size_t m_read = 0;
+        size_t m_write = 0;
+
+    public:
+        CircularBuffer(size_t size) : m_size(size) {} }
+};
+
 class Logger
 {
     private:
