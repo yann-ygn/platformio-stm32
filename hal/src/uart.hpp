dd7b2828b0b05b004b946d46779c9e5931ae94ad
effectspcbs <yann.gaudineau@gmail.com>
2022-03-01 22:36:27 +0100

    20220301/22:36

--- hal/src/uart.hpp
+++ hal/src/uart.hpp
@@ -1,5 +1,5 @@
-#ifndef HAL_DMA_H_
-#define HAL_DMA_H_
+#ifndef HAL_UART_H_
+#define HAL_UART_H_
 
 #include <cstdint>
 
@@ -8,15 +8,39 @@
 
 namespace hal {
   class Usart {
-    struct Config {
-      enum class Periph {
-        Usart1,
-        Usart2,
+    public:
+      struct Config {
+        enum class Periph {
+          Usart1,
+          Usart2,
+        };
+
+        enum class BaudRate {
+          BaudRate9600,
+        };
+
+        Pin txPin;
+        Pin rxPin;
+        Periph periph;
+        BaudRate baud;
+
+        Config() : txPin(),
+                  rxPin(),
+                  periph(Periph::Usart1),
+                  baud(BaudRate::BaudRate9600) {}
       };
 
-      Pin txPin;
-      Pin rxPin;
-    };
+      Usart() = default;
+
+      virtual void setupUsart(Pin t_txPin,
+                              Pin t_rxPin,
+                              Config::Periph t_periph = Config::Periph::Usart1,
+                              Config::BaudRate t_baud = Config::BaudRate::BaudRate9600);
+
+    private:
+      Config m_cfg;
+
+      USART_TypeDef *m_usartPeriph = nullptr;
   };
 }
 
