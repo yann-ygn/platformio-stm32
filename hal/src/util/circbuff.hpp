5a1352715a3f92ea3225850081657eb05d3f2513
effectspcbs <yann.gaudineau@gmail.com>
2022-09-21 21:45:45 +0200

    20220921/21:45

--- hal/src/util/circbuff.hpp
+++ hal/src/util/circbuff.hpp
@@ -9,51 +9,15 @@ namespace util {
     public:
       CircBuff() = default;
 
-      void putItem(T t_item) {
-        m_buffer[m_head] = t_item;
-
-        if (m_full) {
-          m_tail = (m_tail + 1) % m_maxSize;
-        }
-
-        m_head = (m_head + 1) % m_maxSize;
-        m_full = (m_head == m_tail);
-      }
-
-      T getItem() {
-        if (! isEmpty())
-        {
-          T data = m_buffer[m_tail];
-          m_full = false;
-          m_tail = (m_tail + 1) % m_maxSize;
-
-          return data;
-        }
-      }
-
       bool isEmpty() const {
-        return (m_head == m_tail);
+        return (!m_full && (m_head == m_tail));
       }
 
       bool isFull() const {
         return m_full;
       }
 
-      size_t getCapacity() const {
-        size_t size = m_maxSize;
-
-        if (! m_full) {
-          if (m_head >= m_tail) {
-            size = m_head - m_tail;
-          } else {
-            size = m_maxSize + m_head - m_tail;
-          }
-        }
-
-        return size;
-      }
-
-      size_t getMaxSize() const {
+      size_t capacity() const {
         return m_maxSize;
       }
 
