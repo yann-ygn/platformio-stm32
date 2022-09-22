0a77788d3e8b0a5dcb1aa4ed3641d3a44921b400
effectspcbs <yann.gaudineau@gmail.com>
2022-09-22 22:28:18 +0200

    20220922/22:28

--- hal/src/util/circbuff.hpp
+++ hal/src/util/circbuff.hpp
@@ -9,6 +9,33 @@ namespace util {
     public:
       CircBuff() = default;
 
+      void putItem(T t_item) {
+        m_buffer[m_head] = t_item;
+
+        if (isFull()) {
+          m_tail = (m_tail + 1) % m_maxSize;
+        }
+
+        m_head = (m_head + 1) % m_maxSize;
+        m_full = (m_head == m_tail);
+      }
+
+      void putItems(const T* t_items, size_t t_len) {
+        copy(t_items, &m_buffer[m_head], t_len)
+      }
+
+      T getItem() {
+        if (isEmpty()) {
+          return;
+        }
+
+        T item = m_buffer[m_tail];
+        m_full = false;
+        m_tail = (m_tail + 1) % m_maxSize;
+
+        return item;
+      }
+
       bool isEmpty() const {
         return (!m_full && (m_head == m_tail));
       }
@@ -28,5 +55,11 @@ namespace util {
       size_t m_head = 0;
       size_t m_tail = 0;
       bool m_full = false;
+
+      void copy(T* t_dest, const T* t_src, size_t t_len) {
+        while (t_len--) {
+          *t_dest++ = *t_src++;
+        }
+      }
   };
 }
