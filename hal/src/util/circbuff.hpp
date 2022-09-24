20462d2a92c35a1a1e427be39a1b897bd60c7759
effectspcbs <yann.gaudineau@gmail.com>
2022-09-24 23:22:21 +0200

    20220924/23:22

--- hal/src/util/circbuff.hpp
+++ hal/src/util/circbuff.hpp
@@ -4,62 +4,85 @@
 #include <cstddef>
 
 namespace util {
-  template <typename T, size_t S>
-  class CircBuff {
-    public:
-      CircBuff() = default;
 
-      void putItem(T t_item) {
-        m_buffer[m_head] = t_item;
+template <typename T, size_t S>
+class CircBuff {
+  public:
+    CircBuff() = default;
 
-        if (isFull()) {
-          m_tail = (m_tail + 1) % m_maxSize;
-        }
+    void putItem(T t_item) {
+      m_buffer[m_head] = t_item;
 
-        m_head = (m_head + 1) % m_maxSize;
-        m_full = (m_head == m_tail);
-      }
-
-      void putItems(const T* t_items, size_t t_len) {
-        copy(t_items, &m_buffer[m_head], t_len)
-      }
-
-      T getItem() {
-        if (isEmpty()) {
-          return;
-        }
-
-        T item = m_buffer[m_tail];
-        m_full = false;
+      if (isFull()) {
         m_tail = (m_tail + 1) % m_maxSize;
-
-        return item;
       }
 
-      bool isEmpty() const {
-        return (!m_full && (m_head == m_tail));
+      m_head = (m_head + 1) % m_maxSize;
+      m_full = (m_head == m_tail);
+    }
+
+    void putItems(const T* t_items, size_t t_len) {
+      if (t_len > size()) {
+        t_len = size();
       }
 
-      bool isFull() const {
-        return m_full;
+      copy(&m_buffer[m_head], t_items, t_len);
+
+      if (isFull()) {
+        m_tail = (m_tail + 1) % m_maxSize;
       }
 
-      size_t capacity() const {
-        return m_maxSize;
-      }
+      m_head = (m_head + t_len) % m_maxSize;
+      m_full = (m_head == m_tail);
+    }
 
-    private:
-      T m_buffer[S];
+    T getItem() {
+      T item = m_buffer[m_tail];
+      m_full = false;
+      m_tail = (m_tail + 1) % m_maxSize;
 
-      size_t m_maxSize = S;
-      size_t m_head = 0;
-      size_t m_tail = 0;
-      bool m_full = false;
+      return item;
+    }
 
-      void copy(T* t_dest, const T* t_src, size_t t_len) {
-        while (t_len--) {
-          *t_dest++ = *t_src++;
+    bool isEmpty() const {
+      return (!m_full && (m_head == m_tail));
+    }
+
+    bool isFull() const {
+      return m_full;
+    }
+
+    size_t capacity() const {
+      return m_maxSize;
+    }
+
+    size_t size() const {
+      size_t size = m_maxSize;
+
+      if (!isFull()) {
+        if (m_head >= m_tail) {
+          size = m_head - m_tail;
+        }
+        else {
+          size = m_maxSize + m_head -m_tail;
         }
       }
+
+      return size;
+    }
+
+  private:
+    T m_buffer[S];
+
+    size_t m_maxSize = S;
+    size_t m_head = 0;
+    size_t m_tail = 0;
+    bool m_full = false;
+
+    void copy(T* t_dest, const T* t_src, size_t t_len) {
+      while (t_len--) {
+        *t_dest++ = *t_src++;
+      }
+    }
   };
 }
